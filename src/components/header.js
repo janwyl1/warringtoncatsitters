d4d37e6d48cc861066364bfa8b977dca17867d25
James Anwyl <ja.anwyl@gmail.com>
2020-12-01 23:42:51 +0000

    updated navbar

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import {Link} from "gatsby";
 import logo from '../images/warrington-cat-sitters-logo.svg';
 
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default function Header() {
 return (
@@ -34,25 +34,5 @@ return (
             </Navbar.Collapse>
         </Navbar>
     </>
-
-);
-}
-
-
-    // <header class="menu">
-    //     <div class="menu-wrap">
-    //         <a href="./index.html"><img src="./img/warrington_cat_sitters.png" alt="Warrington Cat Sitters Logo"
-    //                 class="logo-img" /></a>
-    //         <input type="checkbox" id="checkbox" />
-    //         <nav>
-    //             <ul>
-    //                 <li><a href="./index.html">Cat Sitting</a></li>
-    //                 <li><a href="./pet-sitting.html">Pet Sitting</a></li>
-    //                 <li><a href="#contact">Contact Us</a></li>
-    //             </ul>
-    //         </nav>
-    //         <label for="checkbox">
-    //             <i class="fa-bars menu-icon"></i>
-    //         </label>
-    //     </div>
-    // </header>
+    )
+};

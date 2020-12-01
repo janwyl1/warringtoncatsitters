f51e85ab122ef027b970fe5426f63695007678a3
James Anwyl <ja.anwyl@gmail.com>
2020-12-01 22:24:06 +0000

    added all bootstrap scss files

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,5 @@
 import React from "react"
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import '../styles/style.scss';
 
 export default function Layout ({children}){
     return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from '../components/layout'
-// import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <Layout>
-      
+      <Button variant="primary">Test</Button>{' '}
     </Layout>
     )
 }

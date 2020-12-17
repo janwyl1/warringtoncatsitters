0ab488e79a364d91dc989cfbaaa5d4eece096bdd
James Anwyl <ja.anwyl@gmail.com>
2020-12-17 05:24:50 +0000

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,58 @@
 import React from "react"
 import Layout from "../components/layout"
 
-import ContactSmall from "../components/contactSmall"
+import {Link} from "gatsby"
 
-import { Container, Row, Col } from "react-bootstrap"
+import ContactSmall from "../components/contactSmall"
+// import Hero from "../components/hero"
+// import LeftAlignedSection from "../components/leftAlignedSection"
+
+import { Container, Row, Col} from "react-bootstrap"
+
+import cat404 from "../images/404-cat.jpg"
+
 
 export default function Home() {
   return (
     <>
       <Layout>
         <Container>
+        {/* <Hero
+            title="Page not found!"
+            tagline="Our expert team is on the case"
+            ctaText="View homepage"
+            heroImg={cat404}
+            heroAlt="Cat playing with computer cables"
+          />
+
+
+        <LeftAlignedSection
+            title="Links"
+          >
+            <p>You may be looking for:</p>
+            <ul>
+                <li><a href="/">Cat Sitting</a></li>
+                <li><a href="/pet-sitting">Pet Sitting</a></li>
+            </ul>
+          </LeftAlignedSection> */}
+
+            <Row className="mb-2">
+                <Col xs={12} lg={{span: 10, offset: 1}} className="text-center">
+                    <h1>Something went wrong!</h1>
+                    <p className="lead mb-2">Sorry, we couldn't find the page you were looking for.</p>
+                </Col>
+            </Row>
             <Row className="mb-4">
-                <Col xs={12} className="text-center">
-                    <h1>Page not found!</h1>
-                    <p>Error code: 404</p>
-                    <p>Our expert team is on the case</p>
-                    <p>You may be looking for one of the following:</p>
-                    <ul>
-                        <li><a href="/">Cat Sitting</a></li>
-                        <li><a href="/pet-sitting">Pet Sitting</a></li>
-                    </ul>
+                <Col xs={12} md={{span: 10, offset: 1}} lg={{span:6, offset:3}} className="text-center">
+                    <a className="btn btn-purple hero-btn d-block mb-4" href="/">Return to homepage</a> 
+                    <span className="text-grey error-code d-block">Error Code: 404</span>
                 </Col>
             </Row>
             <Row>
-
-            </Row> 
+                <Col xs={12} lg={{span:10, offset:1}} className="v-spacer">
+                    <img src={cat404} className="img hero-img" alt="Cat playing with computer cables" />
+                </Col>          
+            </Row>
             <ContactSmall
                 title="Still can't find what your looking for?"
                 lead="Contact us for help:"
